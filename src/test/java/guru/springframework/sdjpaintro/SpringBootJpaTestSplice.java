833a699a6c4b076964416f7a5de00ad421ace3e8
parinitmishra9 <parinitmishra9@gmail.com>
2022-04-21 17:39:45 +0530

    4s_27 data-jpa-test

---
+++ src/test/java/guru/springframework/sdjpaintro/SpringBootJpaTestSplice.java
@@ -0,0 +1,27 @@
+package guru.springframework.sdjpaintro;
+
+import guru.springframework.sdjpaintro.domain.Book;
+import guru.springframework.sdjpaintro.repositories.BookRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@DataJpaTest
+public class SpringBootJpaTestSplice {
+
+    @Autowired
+    BookRepository bookRepository;
+
+    @Test
+    void testJpaTestSplice() {
+        long countBefore = bookRepository.count();
+
+        bookRepository.save(new Book("My Book", "1235555", "Self"));
+
+        long countAfter = bookRepository.count();
+
+        assertThat(countBefore).isLessThan(countAfter);
+    }
+}
